a87b469b69b7a8add10bfda20636a3ef9a0b5577
walkingmask <walkingmask@gmail.com>
2016-10-29 17:27:56 +0900

    fix little

--- webserver/mian.go
+++ webserver/mian.go
@@ -1,41 +1,41 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "text/template"
+	"net/http"
+	"text/template"
 
-  "github.com/jteeuwen/go-pkg-optarg"
+	"github.com/jteeuwen/go-pkg-optarg"
 )
 
+// Page is HTML page
 type Page struct {
-  Title string
-  Count int
+	Title string
+	Count int
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-  page := Page{"Hello World.", 1}
-  tmpl, err := template.ParseFiles("template.html") // ParseFilesを使う
-  if err != nil {
-    panic(err)
-  }
+	page := Page{"Hello World.", 1}
+	tmpl, err := template.ParseFiles("template.html") // ParseFilesを使う
+	if err != nil {
+		panic(err)
+	}
 
-  err = tmpl.Execute(w, page)
-  if err != nil {
-    panic(err)
-  }
+	err = tmpl.Execute(w, page)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-  optarg.Add("p", "port", "Specify port number", "8080")
-  var portNum string = "8080"
-  for opt := range optarg.Parse() {
-    switch opt.ShortName {
-    case "p":
-      portNum = opt.String()
-    }
-  }
+	optarg.Add("p", "port", "Specify port number", "8080")
+	portNum := "8080"
+	for opt := range optarg.Parse() {
+		switch opt.ShortName {
+		case "p":
+			portNum = opt.String()
+		}
+	}
 
-  http.HandleFunc("/", viewHandler) // hello
-  http.ListenAndServe(":"+portNum, nil)
+	http.HandleFunc("/", viewHandler) // hello
+	http.ListenAndServe(":"+portNum, nil)
 }
